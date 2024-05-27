c67b2f2dbd9bb277a06a7b9b36d23271ea4a480d
志成 chen <michael112246@yahoo.com.tw>
2024-05-27 17:33:10 +0800

    first commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows.Forms;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+        /// <summary>
+        /// 顯示員工檔
+        /// 2024/05/23
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetEmps()
+        {
+            DBDao dbdao = new DBDao();
+            List<EMP> emps = dbdao.GetEMPs();
+            ViewBag.emps = emps;
+            return View();
+        }
+
+        /// <summary>
+        /// 新增員工基本檔
+        /// 2024/05/23
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddEMP()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 新增員工基本檔
+        /// 2024/05/23
+        /// </summary>
+        /// <param name="emp"></param>
+        /// <returns></returns>
+       [HttpPost]
+        public ActionResult AddEMP(EMP emp)
+        {
+            DBDao dbdao = new DBDao();
+            try
+            {
+                //新增一筆員工基本檔記錄
+                dbdao.InsertEMP(emp);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return RedirectToAction("GetEmps");
+        }
+
+      
+        /// <summary>
+        /// 編輯員工基本檔-顯示
+        /// 2024/05/23
+        /// </summary>
+        /// <param name="emp_id"></param>
+        /// <returns></returns>
+        public ActionResult EditEMP(string emp_id)
+        {
+            DBDao dbdao = new DBDao();
+            EMP emp = new EMP();
+            try
+            {
+                //編輯一筆員工基本檔記錄
+                emp = dbdao.GetEMPbyID(emp_id);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return View(emp);
+        }
+
+        /// <summary>
+        /// 修改一筆員工基本檔記錄
+        /// 2024/05/23
+        /// </summary>
+        /// <param name="emp"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult EditEMP(EMP emp)
+        {
+            DBDao dbdao = new DBDao();
+            try
+            {
+                //修改一筆員工基本檔記錄
+                dbdao.UpdateEMP(emp);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return RedirectToAction("GetEmps");
+        }
+
+        /// <summary>
+        /// 以代號刪除一筆員工資料
+        /// 2024/05/23
+        /// </summary>
+        /// <param name="emp_id"></param>
+        /// <returns></returns>
+        public ActionResult DeleteEMP(string emp_id)
+        {
+            DialogResult Result = MessageBox.Show("刪除這位員工嗎?", "Confirm Message", MessageBoxButtons.OKCancel);
+            if (Result == DialogResult.OK)
+            {
+              
+                DBDao dbdao = new DBDao();
+                try
+                {
+                    //刪除一筆員工基本檔記錄
+                    dbdao.DeleteEMPbyID(emp_id);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+             
+            }
+            else if (Result == DialogResult.Cancel)
+            {
+                //
+            }
+
+            return RedirectToAction("GetEmps");
+
+        }
+
+
+    }
+    }
---
+++ WebApplication1/Models/EMP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    /// <summary>
+    /// 2024/5/22
+    /// </summary>
+    public class EMP
+    {
+        public string Emp_ID { get; set; }
+        public string Emp_Name { get; set; }
+        public int Age { get; set; }
+        public string Birthday { get; set; }
+    }
+}
