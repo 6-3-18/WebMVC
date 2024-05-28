ff4f5ad726578f0079358f1ae437165b9be93e8f
志成 chen <michael112246@yahoo.com.tw>
2024-05-28 15:17:31 +0800

    [UPD] EMP_ID AUTO INCREMENT

--- EMP.sql
+++ EMP.sql
@@ -1,15 +1,15 @@
 USE [TESTDB]
 GO
 
-/****** Object:  Table [dbo].[EMP]    Script Date: 2024/5/27 ‰∏ãÂçà 01:57:49 ******/
+/****** Object:  Table [dbo].[EMP]    Script Date: 2024/5/28 §U§» 12:46:55 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[EMP](
-	[EMP_ID] [varchar](50) NOT NULL,
+CREATE TABLE [dbo].[EMP](	
+	[EMP_ID] [int] IDENTITY(1,1) NOT NULL,
 	[EMP_NAME] [varchar](50) NOT NULL,
 	[AGE] [int] NOT NULL,
 	[BIRTHDAY] [varchar](50) NOT NULL,
@@ -19,4 +19,5 @@ CREATE TABLE [dbo].[EMP](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
+
 
